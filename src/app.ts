62df6b7d88a39e6a2548ed7541d91c4cb0a559b5
Hr-Sajib <hrsajib001@gmail.com>
2025-06-13 23:04:53 +0600

    project initiated

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import express, { Application, Request, Response } from "express";
+import globalErrorHandler from "./app/middlewares/globalErrorhandler";
+// import cors from "cors";
+const app: Application = express();
+
+// parser
+  app.use(express.json());
+  // app.use(cors({origin: [
+  //     "http://localhost:5173",  // Local development
+  //     "https://bicycle-store-client-one.vercel.app",  // Vercel frontend
+  //   ], credentials: true}));
+
+
+// application routes 
+   
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("TaskFlow server running ..");
+});
+
+
+app.use(globalErrorHandler);
+
+
+export default app;
