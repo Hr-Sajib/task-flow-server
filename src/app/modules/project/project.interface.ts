5d80df0e5245f93957f06f519d33bbbf70ff3550
Hr-Sajib <hrsajib001@gmail.com>
2025-06-14 01:15:50 +0600

    project interface and model schema added

---
+++ src/app/modules/project/project.interface.ts
@@ -0,0 +1,27 @@
+export interface IProject {
+    _id: string;
+    projectId: string;
+    projectName: string;
+    station: string;
+    clientId: string;
+    deadline: Date;
+    isCanceled: boolean;
+    cancellationNote: string;
+    teamName: string; // আপাতত string রাখা হয়েছে যদিও পরবর্তীতে team এর module কমপ্লিট হবার পর সেখান থেকে ডায়নামিক অপশন ইউজ করা হবে 
+    frontendRoleAssignedTo: string; // আপাতত string রাখা হয়েছে যদিও পরবর্তীতে team এর module কমপ্লিট হবার পর সেখান থেকে ডায়নামিক অপশন ইউজ করা হবে 
+    backendRoleAssignedTo: string; // আপাতত string রাখা হয়েছে যদিও পরবর্তীতে team এর module কমপ্লিট হবার পর সেখান থেকে ডায়নামিক অপশন ইউজ করা হবে 
+    uiRoleAssignedTo: string; // আপাতত string রাখা হয়েছে যদিও পরবর্তীতে team এর module কমপ্লিট হবার পর সেখান থেকে ডায়নামিক অপশন ইউজ করা হবে 
+    lastUpdate: Date;
+    lastMeeting: Date;
+    projectStatus: "ui/ux" | "wip" | "new" | "qa" | "delivered" | "revision";
+    estimatedDelivery: "thisMonth" | "thisWeek" | "nextMonth";
+    rating: "1" | "2" | "3" | "4" | "5" | "noRating";
+    clientStatus: "active" | "inactive" | "satisfied" | "unsatisfied" | "angry" | "neutral";
+    figmaLink: string;
+    backendLink: string;
+    liveLink: string;
+    deliveryDate: Date;
+    requirementDoc: string;
+    notes: Array<{ noteProvider: string; noteText: string }>;
+}
+
---
+++ src/app/modules/project/project.model.ts
@@ -0,0 +1,104 @@
+import { model, Schema } from "mongoose";
+import { IProject } from "./project.interface";
+
+const projectSchema = new Schema<IProject>(
+  {
+    projectId: {
+      type: String,
+      required: [true, "Project ID is required"],
+    },
+    projectName: {
+      type: String,
+      required: [true, "Project name is required"],
+    },
+    station: {
+      type: String,
+      required: [true, "Station name / Project source is required"],
+    },
+    clientId: {
+      type: String,
+      required: [true, "Client ID is required"],
+    },
+    deadline: {
+      type: Date,
+      required: [true, "Deadline is required"],
+    },
+    isCanceled: {
+      type: Boolean,
+      default: false,
+    },
+    cancellationNote: {
+      type: String,
+    },
+    teamName: {
+      type: String,
+      required: [true, "Team name is required"],
+    },
+    frontendRoleAssignedTo: {
+      type: String,
+    },
+    backendRoleAssignedTo: {
+      type: String,
+    },
+    uiRoleAssignedTo: {
+      type: String,
+    },
+    lastUpdate: {
+      type: Date,
+    },
+    lastMeeting: {
+      type: Date,
+    },
+    projectStatus: {
+      type: String,
+      enum: ["ui/ux", "wip", "new", "qa", "delivered", "revision"],
+      default: "new",
+      required: [true, "Project status is required"],
+    },
+    estimatedDelivery: {
+      type: String,
+      enum: ["thisMonth", "thisWeek", "nextMonth"],
+      required: [true, "Estimated delivery is required"],
+    },
+    rating: {
+      type: String,
+      enum: ["1", "2", "3", "4", "5", "noRating"],
+      default: "noRating",
+    },
+    clientStatus: {
+      type: String,
+      enum: ["active", "inactive", "satisfied", "unsatisfied", "angry", "neutral"],
+      default: "neutral",
+    },
+    figmaLink: {
+      type: String,
+    },
+    backendLink: {
+      type: String,
+    },
+    liveLink: {
+      type: String,
+    },
+    deliveryDate: {
+      type: Date,
+    },
+    requirementDoc: {
+      type: String,
+    },
+    notes: [
+      {
+        noteProvider: {
+          type: String,
+          required: [true, "Note provider is required"],
+        },
+        noteText: {
+          type: String,
+          required: [true, "Note text is required"],
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export const ProjectModel = model<IProject>("Project", projectSchema);
